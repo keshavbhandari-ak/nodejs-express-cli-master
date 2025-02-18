f11af1050966bffba26e8d45d0f9430b3749667d
keshavbhandaridev <keshavbhandaridev@gmail.com>
2025-02-18 14:39:37 +0530

    feat: Create function to run commands and generate package.json using it

--- src/operations/projectGenerator/projectGenerator.ts
+++ src/operations/projectGenerator/projectGenerator.ts
@@ -1,16 +1,32 @@
 import fs from "fs-extra";
 import path from "path";
 import { ProjectDetailsConfig } from "../../types";
+import CommandLineService from "../../services/commandLineService";
 
-export async function generateProject(projectDetails: ProjectDetailsConfig) {
-    const { projectName } = projectDetails;
+const createProjectDirectory = async (projectName: string) => {
     const currentDir = process.cwd();
-    const testProjectDir = path.join(currentDir, projectName);
-
+    const projectDirectory = path.join(currentDir, projectName);
     try {
-        await fs.ensureDir(testProjectDir);
-        console.log(`${projectName} folder created at: ${testProjectDir}`);
+        await fs.ensureDir(projectDirectory);
+        console.log(`${projectName} folder created at: ${projectDirectory}`);
     } catch (err) {
         console.error("Error:", err);
     }
+    return projectDirectory;
+};
+
+const generatePackageJson = async () => {
+    try {
+        const commandLineObj = CommandLineService.getInstance();
+        await commandLineObj.executeCommand("npm", ["init", "-y"]);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+export async function generateProject(projectDetails: ProjectDetailsConfig) {
+    const { projectName } = projectDetails;
+    const projectDirectory = await createProjectDirectory(projectName);
+    process.chdir(projectDirectory);
+    await generatePackageJson();
 }
--- src/services/commandLineService.ts
+++ src/services/commandLineService.ts
@@ -1,4 +1,5 @@
 import { input } from "@inquirer/prompts";
+import { spawn } from "child_process";
 
 class CommandLineService {
     private static instance: CommandLineService;
@@ -16,6 +17,22 @@ class CommandLineService {
         return await input({
             message,
             default: defaultValue ?? "Test"
+        });
+    }
+
+    async executeCommand(command: string, args: string[]) {
+        return new Promise<void>((resolve, reject) => {
+            const npmInit = spawn(command, args);
+
+            npmInit.on("close", (code: number) => {
+                const commandString = `${command} ${args.join(" ")}`;
+                if (code === 0) {
+                    console.log(`${commandString} process completed successfully`);
+                    resolve();
+                } else {
+                    reject(new Error(`${commandString} process exited with code ${code}`));
+                }
+            });
         });
     }
 }
